d065f553f771753a50f975820bbfc33e6004a25c
Luke Anger <112991266+LukeAnger@users.noreply.github.com>
2024-12-17 12:28:10 -0500

    updated request

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,7 +1,24 @@
 import { HOST_URL } from "./config"
+const requestBody = {
+    "method": "GET",
+    "port": "9004",
+    "path": "feedback_generation/ta_interface/",
+    "params": {
+        "action": "fetch_one",
+        "TA_id": "luang"
+    }
+}
+
 
 export const getOneHF = async () => {
-    const response = await fetch(HOST_URL + 'feedback_generation/getOne')
+    const response = await fetch(HOST_URL, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
     const data = await response.json()
     return data
 }
